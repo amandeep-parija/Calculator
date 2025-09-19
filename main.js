4af97659e601a4a32a621b1283ef5ecf24a0e745
amandeep-parija <93464533+amandeep-parija@users.noreply.github.com>
2025-09-19 19:41:12 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// let string = "";
+// let buttons = document.querySelectorAll('.button');
+// Array.from(buttons).forEach((button) =>{
+//     button.addEventListener('click', (e)=>{
+//         if(e.target.innerHTML == '='){
+//             string = eval(string);
+//             document.querySelector('input').value = string;
+//         }else if(e.target.innerHTML == 'CE'){
+//             string = '';
+//             document.querySelector('input').value = string;
+//         }else if(e.target.innerHTML == 'E'){
+//             string = '';
+//             document.querySelector('input').value = string;
+//         }else if(e.target.innerHTML == 'E'){
+//             string = '';
+//             document.querySelector('input').value = string;
+//         }else{
+//             console.log(e.target)
+//             string= string + e.target.innerHTML;
+//             document.querySelector('input').value = string;
+//         }
+//     })
+// })
+
+
+
+let string = "";
+
+let buttons = document.querySelectorAll( '.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener("click", (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }else if(e.target.innerHTML == 'C'){
+            string = '';
+            document.querySelector('input').value = string;
+        }else if(e.target.innerHTML == 'CE'){
+            string = '';
+            document.querySelector('input').value = string;
+        }else if(e.target.innerHTML == 'C'){
+            string = '';
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == '<i class="fa-solid fa-delete-left"></i>'){
+            string = '';
+            document.querySelector('input').value = string;
+        }else{
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+        }
+    })
+})
