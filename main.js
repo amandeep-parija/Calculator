8c21b3822bb3bca2a7caedb18c7f66d94efd151a
amandeep-parija <93464533+amandeep-parija@users.noreply.github.com>
2025-09-22 16:48:15 +0530

    Only logic

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let string = "";
 
 let buttons = document.querySelectorAll( '.button');
-Array.from(buttons).forEach((button)=>{
+    buttons.forEach((button)=>{
     button.addEventListener("click", (e) =>{
         if(e.target.innerHTML == '='){
             string = eval(string);
@@ -9,10 +9,13 @@ Array.from(buttons).forEach((button)=>{
         }else if(e.target.innerHTML == 'C'){
             string = '';
             document.querySelector('input').value = string;
-        }else if(e.target.innerHTML == 'CE'){
-            string = '';
+        }else if(e.target.innerHTML == '<i class="fa-solid fa-delete-left"></i>'){
+            string = string.slice(0, -1);
             document.querySelector('input').value = string;
         }else if(e.target.innerHTML == 'C'){
+            string = '';
+            document.querySelector('input').value = string;
+        }else if(e.target.innerHTML == 'CE'){
             string = '';
             document.querySelector('input').value = string;
         }else{
@@ -22,42 +25,6 @@ Array.from(buttons).forEach((button)=>{
         }
     })
 })
-
-function storeOutput(){
-    if(string){
-    let value = JSON.parse(localStorage.getItem("value")) || [];
-    value.push({ string})
-    localStorage.setItem("string", JSON.stringify(string));
-}
-
-function showOutput(){
-    let value = JSON.parse(localStorage.getItem("value")) || [];
-    let output = "<h3>Calculation history</h3>";
-
-    if (value.length > 0) {
-    output += "<table><tr><th>History</th></tr>";
-    credentials.forEach((item) => {
-      output += `<tr>
-                   <td>${item.value}</td>
-                 </tr>`;
-    });
-    output += "</table>";
-     } else {
-    output += "<p>There is no history.</p>";
-  }
-document.getElementById("output").innerHTML = output;
-
-}
-function clearOutput() {
-  localStorage.removeItem("value");
-  document.getElementById("output").innerHTML = "<p>There is no history.!</p>";
-}
-}
-
-
-document.getElementById("btnSave").addEventListener("click",  storeOutput);
-document.getElementById("btnShow").addEventListener("click",  showOutput);
-document.getElementById("btnClear").addEventListener("click", clearOutput);
 
 
 
